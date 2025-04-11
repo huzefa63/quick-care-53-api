adb6473cdd269b402324572c0d260f8909e579fa
learner <huzefaratlam63@gmail.com>
2025-04-11 18:20:48 +0530

    Update appointmentRoutes.js

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -13,42 +13,8 @@ route.post('/checkSlot',checkSlot);
 route.post('/getAppointmentById',getAppointmentById);
 route.post('/removeAppointment',removeAppointment);
 route.post('/checkout-session',getCheckoutSession);
-route.post(
-  "/webhook",
-  express.raw({ type: "application/json" }),
-  async (req, res) => {
-    const sig = req.headers["stripe-signature"];
-    let event;
 
-    try {
-      event = stripe.webhooks.constructEvent(
-        req.body,
-        sig,
-        `${process.env.STRIPE_WEBHOOK_SECRET}`
-      );
-    } catch (err) {
-      console.error("Webhook signature verification failed:", err.message);
-      return res.status(400).send(`Webhook Error: ${err.message}`);
-    }
 
-    // Handle successful checkout session
-    if (event.type === "checkout.session.completed") {
-      const session = event.data.object;
-
-      if (session.payment_status === "paid") {
-
-        // TODO: Call your function to save appointment, e.g.
-        const appointment = {
-           date: session.metadata.date,
-      doctor: session.metadata.doctor,
-      userId: session.metadata.userId,
-      status: session.metadata.status,
-      fee: session.metadata.fee,
-        };
-        await Appointment.create(appointment);
-        // createAppointment({ doctorId, userId, appointmentTime });
-      }
-    }
 
     res.status(200).json({ received: true });
   }
